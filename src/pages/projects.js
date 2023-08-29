c902e8233598858724ac3203193d2baac33cde06
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2023-08-29 13:58:45 +0530

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -210,7 +210,7 @@ const projects = () => {
                           project
                             project
                               project
-                                project
+             
                          
                           
                                
