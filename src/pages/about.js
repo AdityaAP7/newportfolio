8c23f31b071b9dd1932c88b573b7c5c7f9ba44f2
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2023-09-06 23:52:10 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,16 +2,7 @@
         >
             {
                 cast && cast.map((person, index)=>{
-                    return (
-                        <TouchableOpacity 
-                            key={index} 
-                            onPress={()=> navigation.navigate('Person', person)} 
-                            className="mr-4 items-center">
-                            <View 
-                                className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
-                                <Image 
-                                    className="rounded-2xl h-24 w-20"
-                                    // source={require('../assets/images/castImage1.png')} 
+         \)} 
                                     source={{uri: image185(person?.profile_path) || fallbackPersonImage}} 
                                 />
                             </View>
